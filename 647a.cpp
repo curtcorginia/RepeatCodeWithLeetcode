b8d026ab554a8419653c278b253af4e12d11eb9c
evansoohoo <evan.soohoo@gmail.com>
2021-09-29 07:34:59 -0700

    initial commit

---
+++ 647a.cpp
@@ -0,0 +1,40 @@
+//Based on the work of keshavk https://leetcode.com/problems/palindromic-substrings/discuss/105688/Very-Simple-Java-Solution-with-Detail-Explanation
+
+class Solution {
+public: 
+    
+    int countFromCenter(std::string& inputStr, int startInd, int endInd)
+        
+    {
+        int addNum = 0;
+        if(inputStr.size() == 0)
+        {
+            return addNum;
+        }        
+        while( startInd >= 0 && endInd < inputStr.size() 
+              && inputStr[startInd] == inputStr[endInd])
+        {
+            addNum++;
+            startInd--;
+            endInd++;
+            
+        }
+        return addNum;
+    }
+    int countSubstrings(string s) {
+        int finalCount = 1;
+        if(s.size() == 0)
+        {
+            return 0;
+        }
+        for(int i = 0; i < s.size() - 1; i++)
+        {
+            
+            finalCount += countFromCenter(s, i, i);
+            finalCount += countFromCenter(s, i, i + 1);
+        }
+        
+        return finalCount;
+        
+    }
+};
